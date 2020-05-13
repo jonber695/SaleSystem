9b1081f4df013f7ba02b62448c6d100c37e4ba95
jonber695 <64254502+jonber695@users.noreply.github.com>
2020-05-13 14:44:54 +0200

    Added testing for exception handling

--- src/test/java/se/kth/IV1350/jonatbj/model/SaleTest.java
+++ src/test/java/se/kth/IV1350/jonatbj/model/SaleTest.java
@@ -6,8 +6,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import se.kth.IV1350.jonatbj.exception.InventoryNotRespondingException;
 import se.kth.IV1350.jonatbj.integration.ExternalInventorySystem;
 import java.time.*;
+import se.kth.IV1350.jonatbj.exception.*;
 
 /**
  *
@@ -17,42 +20,45 @@ public class SaleTest {
     LocalDateTime localTime = LocalDateTime.now();
     
     /**
-     * Test of the registerItem method, of class Sale, with a "null string"
+     * Test of the registerItem method, of class Sale, with the hardcoded number that makes the method
+     * throw Inventory not responding exception
      */
     @Test
-    public void testRegisterItemWithANullItemID()
+   public void testRegisterItemWhenInventoryIsNotResponding()
     {
         Sale instance = new Sale();
-        int s = 7;
-        instance.registerItem(s, new ExternalInventorySystem());
-        assertTrue(instance.getItems().isEmpty(), "List is empty");
+        int s = 4;
+        try
+        {
+            instance.registerItem(s, new ExternalInventorySystem());
+            fail("Inventory system is working");
+        }
+        catch(InventoryNotRespondingException e)
+        {
+            assertTrue(e.getMessage() == "Inventory system is not responding", "it's working");
+        }
     }
     
     /**
-     * Test of registerItem method, when it works as it is supposed to
+     * Test of getitem method, When there is a invalid item ID sent to it
      */
     @Test
-    public void testRetgisterItem()
+    public void testGetItemWithInvalidItemID()
     {
-        Sale instance = new Sale();
-        int i = 1;
-        Item item = new Item(1, "Milk", (float )10.5, "1 L of milk", (float) .12, 1);
-        instance.registerItem(i, new ExternalInventorySystem());
-        assertTrue(instance.getItems().get(0).getItemID() == item.getItemID(), "this works");
+        ExternalInventorySystem instance = new ExternalInventorySystem();
+        int i = 6;
+        try
+        {
+            instance.getItem(i);
+            fail("The Id was not invalid");
+        }
+        catch(ItemNotInInventoryException e)
+        {
+            assertTrue(e.getMessage() == "The itemID does not exist, no item was added", "It is working");
+        }
     }
     
-    /**
-     * Test of registerItem to see if quantity updates
-     */
-    @Test
-    public void testRegisterItemForUpdatesQuantity()
-    {
-        Sale instance = new Sale();
-        int checkToSeeIfQuantityIsRight = 2;
-        instance.registerItem(1, new ExternalInventorySystem());
-        instance.registerItem(1, new ExternalInventorySystem());
-        assertTrue(instance.getItems().get(0).getQuantity() == checkToSeeIfQuantityIsRight, "Quantity is right");
-    }
+    
 
 
 
